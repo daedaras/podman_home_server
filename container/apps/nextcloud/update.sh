67594b851ddac3d7db111493df4de8bac8f72d4c
daedaras <daedaras@gmail.com>
2025-03-08 13:41:25 +0100

    fix nextcloud update issues

--- container/apps/nextcloud/update.sh
+++ container/apps/nextcloud/update.sh
@@ -35,14 +35,12 @@ occ="podman exec -it nextcloud-php php82 /nextcloud/web/occ"
 upgrade () {
    $occ upgrade | sed -r 's/\x1B\[[0-9;]*[mK]//g' | tr -d '\r\n'
 }
-$occ maintenance:mode --on
 runs=0
 result=""
-while [ "$result" != "Nextcloud is already latest version" ] && [ $runs -lt 6 ]; do
+while [ "$result" != "Nextcloud is already latest version" ] && [ $runs -lt 3 ]; do
     result="$(upgrade)"
     runs=$((runs + 1))
     sleep 10
 done
 $occ db:add-missing-indices
-sleep 20
 $occ maintenance:mode --off
